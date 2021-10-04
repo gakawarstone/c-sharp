feb8dbbacc0418f8528e68600d3b7582d78d9763
gakawarstone <pinnekerv@gmail.com>
2021-10-04 20:51:06 +0600

    18 commit

---
+++ CS1/5/5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static int Max_2(int a, int b)
+        {
+            if (a >= b) {
+                return a;
+            } else {
+                return b;
+            }
+        }
+
+        static bool Replace(ref int a, ref int b)
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+            return true;
+        }
+
+        static int Fact(int a)
+        {
+            int answ;
+            answ = 1;
+            checked
+            {
+                while (a > 0)
+                {
+                    answ *= a;
+                    a -= 1;
+                }
+            }
+
+            return answ;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Max_2(2, 3));
+
+            int a = 1;
+            int b = 2;
+
+            Replace(ref a, ref b);
+            Console.WriteLine($"{a}:{b}");
+
+            Console.WriteLine(Fact(3));
+        }
+    }
+}
