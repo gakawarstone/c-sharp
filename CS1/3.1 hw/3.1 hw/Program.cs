38ee7c73fd25419c704fac4f85b8c449fff4050c
gakawarstone <pinnekerv@gmail.com>
2021-10-04 19:02:59 +0600

    16 commit

---
+++ CS1/3.1 hw/3.1 hw/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3._1_hw
+{
+    class Program
+    {
+        enum uni { KGU, KSTU, KAI }
+
+        struct employer
+        {
+            public string name;
+            public uni uni;
+        }
+        static void Main(string[] args)
+        {
+            employer Ivan;
+            Ivan.name = "Ivan";
+            Ivan.uni = (uni)1;
+            Console.WriteLine($"Name: {Ivan.name}");
+            Console.WriteLine($"Uni: {Ivan.uni}");
+        }
+    }
+}
