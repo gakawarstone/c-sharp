192c7df13b0b3f06779d7ff9abde220a617b119e
gakawarstone <pinnekerv@gmail.com>
2021-10-04 18:37:13 +0600

    14 commit

---
+++ CS1/3.1/3.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3._1
+{
+    enum account_type { Regular, Savings }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            account_type first, second;
+            first = (account_type)0;
+            second = (account_type)1;
+            Console.WriteLine($"first account is {first}");
+            Console.WriteLine($"second account is {second}");
+        }
+    }
+}
