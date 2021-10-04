1063a620b1790863a38f3b0dd6f047be62c88d99
gakawarstone <pinnekerv@gmail.com>
2021-10-04 10:55:01 +0600

    13 commit

--- CS1/2.2 hw/2.2 hw/Program.cs
+++ CS1/2.2 hw/2.2 hw/Program.cs
@@ -6,32 +6,33 @@ namespace _2._2_hw
     {
         static void Main(string[] args)
         {
-            int a, b, c;
-            int d;
-            int x1, x2;
+            float a, b, c;
+            float d, d_sqrt;
+            float x1, x2;
 
             Console.WriteLine("ax^2 + bx + c = 0");
             Console.WriteLine("print a");
-            a = (int)Console.ReadLine();
+            a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("print b");
-            b = (int)Console.ReadLine();
+            b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("print c");
-            c = (int)Console.ReadLine();
+            c = Convert.ToInt32(Console.ReadLine());
 
-            d = b**2 - 4*a*c;
+            d = b*b - 4*a*c;
+            d_sqrt = MathF.Sqrt(d);
 
             if (d > 0) {
-                x1 = (-b + d**0.5) / 2*a
-                x2 = (-b + d**0.5) / 2*a
+                x1 = (-b + d_sqrt) / 2 * a;
+                x2 = (-b - d_sqrt) / 2 * a;
                 Console.WriteLine($"x1: {x1}; x2: {x2}");
             } else if (d == 0) {
-                x1 = (-b) / 2*a
-                x2 = (-b) / 2*a
+                x1 = (-b) / 2 * a;
+                x2 = (-b) / 2 * a;
                 Console.WriteLine($"x1: {x1}; x2: {x2}");
             } else if (d < 0) {
                 Console.WriteLine("haven't x1 x2");
             } else {
-                Console.WriteLine("ERRROR")
+                Console.WriteLine("ERRROR");
             }
         }
     }
