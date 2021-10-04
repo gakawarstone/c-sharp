424fa32f83223799aa8a8d649a7a3eab888a06f5
gakawarstone <pinnekerv@gmail.com>
2021-10-04 10:15:41 +0600

    10 commit

--- CS1/2.1 hw/2.1 hw/Program.cs
+++ CS1/2.1 hw/2.1 hw/Program.cs
@@ -9,8 +9,8 @@ namespace _2._1_hw
             char key;
 
             Console.WriteLine("Write letter: ");
-            key = Convert.ToChar(Console.Read());
-            key = Convert.ToChar(Convert.ToInt32(key) + 1);
+            key = (char)Console.Read();
+            key = (char)((int)key + 1);
             Console.WriteLine("Next letter is " + key);
         }
     }
