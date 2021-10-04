41492602f518f819cfa9631925f1fc9739d9297d
gakawarstone <pinnekerv@gmail.com>
2021-10-04 10:39:05 +0600

    12 commit

--- CS1/2.2 hw/2.2 hw/Program.cs
+++ CS1/2.2 hw/2.2 hw/Program.cs
@@ -6,7 +6,33 @@ namespace _2._2_hw
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a, b, c;
+            int d;
+            int x1, x2;
+
+            Console.WriteLine("ax^2 + bx + c = 0");
+            Console.WriteLine("print a");
+            a = (int)Console.ReadLine();
+            Console.WriteLine("print b");
+            b = (int)Console.ReadLine();
+            Console.WriteLine("print c");
+            c = (int)Console.ReadLine();
+
+            d = b**2 - 4*a*c;
+
+            if (d > 0) {
+                x1 = (-b + d**0.5) / 2*a
+                x2 = (-b + d**0.5) / 2*a
+                Console.WriteLine($"x1: {x1}; x2: {x2}");
+            } else if (d == 0) {
+                x1 = (-b) / 2*a
+                x2 = (-b) / 2*a
+                Console.WriteLine($"x1: {x1}; x2: {x2}");
+            } else if (d < 0) {
+                Console.WriteLine("haven't x1 x2");
+            } else {
+                Console.WriteLine("ERRROR")
+            }
         }
     }
 }
